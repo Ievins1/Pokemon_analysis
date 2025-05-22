5036a3e00188129d9dda68d133c23dd46b5a2643
Rihards <rievins72@gmail.com>
2025-05-22 12:34:50 +0300

    added .env and readme

---
+++ README.md
@@ -0,0 +1,8 @@
+
+
+
+# Palaišanas instrukcija
+### Lai veiksmīgi palaistu projektu, nepieciešams uzinstalēt pakotnes:
+'pip3 install pandas python-dotenv requests'
+
+### 
--- utils/receive_data.py
+++ utils/receive_data.py
@@ -4,11 +4,11 @@ import pandas as pd
 from dotenv import load_dotenv
 from io import StringIO
 
-# Ielādē .env failā saglabāto mainīgo
+# Receive .env file
 env_path = os.path.join(os.path.dirname(__file__), '..', '.env')
 load_dotenv(dotenv_path=env_path)
 
-# Funkcija, kas atgriež datus, no dotās saites
+# Function that returns data from the given link
 def fetch_pokemon_data():
     url = os.getenv("data_url")
     if not url:
@@ -20,13 +20,13 @@ def fetch_pokemon_data():
 
     # Pārvērš CSV tekstu par pandas DataFrame
     csv_data = StringIO(response.text)
-    df = pd.read_csv(csv_data, on_bad_lines='skip')  # Ignorē nepareizās rindas - piemēram, 1043 rindā ir par daudz vērtības dotas
+    df = pd.read_csv(csv_data, on_bad_lines='skip')  # Ignore the incorrect rows of data, for example, 1043 row has too many values
 
     return df
 
 # Pārbaudei, ka dati tiek atgriezti
 if __name__ == "__main__":
     df = fetch_pokemon_data()
-    print(df.head())
+    print(df.head()) 
     print(f"Atgriezto rindas skaits: {len(df)}")
     print(f"Kolonnu skaits: {len(df.columns)}")
